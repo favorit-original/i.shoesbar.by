8e098a9678d9e9f3e86de15a36961865eb323450
favorit-original <favorit_original@mail.ru>
2021-12-21 19:18:35 +0300

    new

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
-<?php
-
-require __DIR__ . '/auth.php';
-$login = getUserLogin();
-
-?>
-<?php
-if (!empty($_POST)) {
-    require __DIR__ . '/auth.php';
-
-    $login = $_POST['login'] ?? '';
-    $password = $_POST['password'] ?? '';
-
-    if (checkAuth($login, $password)) {
-        setcookie('login', $login, 0, '/');
-        setcookie('password', $password, 0, '/');
-        header('i.shoesbar.by/page.php');
-    } else {
-        $error = 'Ошибка авторизации';
-    }
-}
-?>
+<?php include __DIR__.'/config.php'; ?>
 
 <html>
 <head>
@@ -41,33 +20,36 @@ integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4
     <!-- Login -->
     <div class="login signin">
 	<div class="login_logo"></div>
-	<?php if ($login === null): ?>
-    
-<?php if (isset($error)): ?>
-<span style="color: red;">
-    <?= $error ?>
-</span>
-<?php endif; ?>
-	<form id="myform" method="post">
+	
+    <?php if(AUTH) { //Если мы авторизированы ?>
+
+    Привет, <?php echo $user['name']; ?>!
+    <a href="login.php?logout">Выход</a>
+
+<?php } else { //Если мы не авторизированы  ?>
+        
+	<form action="login.php" id="myform" method="post">
 		<input id="login" class="anim" name="login" placeholder="Email or login" size="20" maxlength="30" value="" tabindex="101" autocapitalize="none" autocorrect="off" required="" autofocus="" type="text">
 		<input id="password" class="anim" name="password" placeholder="Password" size="20" maxlength="60" value="" tabindex="102" autocapitalize="none" autocorrect="off" required="" type="password">
 		<div id="recaptcha-signin" class="g-recaptcha" style="margin: auto; width: 65%; display: none;"><div style="width: 164px; height: 144px;"><div><iframe title="reCAPTCHA" src="https://www.google.com/recaptcha/api2/anchor?ar=1&amp;k=6Ldwng0TAAAAABBCsQx5eswEBBH-94W2_8FIoPJS&amp;co=aHR0cHM6Ly9pbm1hYy5vcmc6NDQz&amp;hl=ru&amp;type=image&amp;v=VZKEDW9wslPbEc9RmzMqaOAP&amp;theme=dark&amp;size=compact&amp;cb=scelj4hnf7f0" width="164" height="144" role="presentation" name="a-v0ats9uqad3c" frameborder="0" scrolling="no" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"></iframe></div><textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"></textarea></div><iframe style="display: none;"></iframe></div>
 		<div class="status-signin" style="color: #b7c0c5; /* font-size: 11px; */">You must enter the name and password</div>
-				<ul class="login_buttons">
+            <?php if(!empty($message)) { ?>
+    <p><?php echo $message; ?></p>
+    <?php } ?>
+        <ul class="login_buttons">
+                    
 			<li>
 			<a href="" onclick="document.getElementById('myform').submit(); return false;" title="Log in" class="next anim fa fa-arrow-right"></a>
 			
 			</li>
 		</ul>
 	</form>
+        
+<?php } ?>
+        
 </div>
 
     <script src="./js/auth.07c0212e8cca2.js"></script>    
     
-<?php else: ?>
-Добро пожаловать, <?= $login ?>
-<br>
-<a href="/logout.php">Выйти</a>
-<?php endif; ?>
 </body>
 </html>
--- usersDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    ['login' => '1', 'password' => '1'],
-    ['login' => 'moderator', 'password' => 'password'],
-    ['login' => 'user', 'password' => '123'],
-];
