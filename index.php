d20f4327c4a32140fba90c13e977c2e6aa71a0db
favorit-original <favorit_original@mail.ru>
2021-12-21 19:40:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -53,3 +53,34 @@ integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4
     
 </body>
 </html>
+
+
+
+
+<html>
+<head>
+    <title>Пример авторизации на сайте</title>
+</head>
+<body>
+
+<?php if(AUTH) { //Если мы авторизированы ?>
+
+    Привет, <?php echo $user['name']; ?>!
+    <a href="login.php?logout">Выход</a>
+
+<?php } else { //Если мы не авторизированы  ?>
+
+<form action="login.php" method="post">
+    <p>Имя пользователя: <input type="text" name="login" /></p>
+    <p>Пароль: <input type="password" name="password" /></p>
+    <p>Запомнить меня: <input type="checkbox" name="remember" /></p>
+    <?php if(!empty($message)) { ?>
+    <p><?php echo $message; ?></p>
+    <?php } ?>
+    <p><input type="submit" value="Вход" /></p>
+</form>
+
+<?php } ?>
+
+</body>
+</html>
